caf0b2829e0f05b33c06e94d1371c8abe52c4ebc
Pitt <Pitterling@users.noreply.github.com>
2017-04-18 00:33:10 +0200

    Improve/stats_handling (#75)
    * refactor lambda expression - get rid of tmp object - we already have the current STAT
    
    * clean class - remove orphaned methods and variables

--- src/main/java/net/atomique/ksar/XML/StatConfig.java
+++ src/main/java/net/atomique/ksar/XML/StatConfig.java
@@ -14,40 +14,16 @@ public class StatConfig {
         this.GraphName = GraphName;
     }
 
-    public String[] getHeader() {
-        return Header;
-    }
-
     public String getHeaderStr() {
         return HeaderStr;
     }
 
     public void setHeaderStr(String s) {
         HeaderStr = s;
-        this.Header = HeaderStr.split("\\s+");
-        HeaderNum = Header.length;
     }
 
     public String getStatName() {
         return StatName;
-    }
-
-    public boolean check_Header(String c, int i) {
-        if (!compare_Header(i)) {
-            return false;
-        }
-
-        if (HeaderStr.equals(c)) {
-            return true;
-        }
-        return false;
-    }
-
-    private boolean compare_Header(int i) {
-        if (i == HeaderNum) {
-            return true;
-        }
-        return false;
     }
 
     public boolean canDuplicateTime() {
@@ -61,10 +37,8 @@ public class StatConfig {
     }
 
 
-    private int HeaderNum = 0;
     private String StatName = null;
     private String GraphName = null;
-    private String Header[] = null;
     private String HeaderStr = null;
     private boolean duplicatetime = false;
 }
