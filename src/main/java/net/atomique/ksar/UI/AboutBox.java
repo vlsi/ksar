53bd9a623ac7d9c149a993c93ea85005f5503755
Peter Pitterling <peter.pitterling@sap.com>
2017-04-22 23:55:31 +0200

    make URL clickable and open link

--- src/main/java/net/atomique/ksar/UI/AboutBox.java
+++ src/main/java/net/atomique/ksar/UI/AboutBox.java
@@ -11,7 +11,15 @@
 
 package net.atomique.ksar.UI;
 
+import java.awt.Cursor;
+import java.awt.Desktop;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.awt.Dimension;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import javax.swing.JLabel;
 import net.atomique.ksar.VersionNumber;
 
 /**
@@ -88,18 +96,24 @@ public class AboutBox extends javax.swing.JDialog {
 
         url ="https://github.com/vlsi/ksar";
         urllabel.setText(String.format("<html> Website :   <a href=\"\">%s</a></html>", url ));
+        urllabel.setCursor(new Cursor(Cursor.HAND_CURSOR));
         jPanel2.add(urllabel);
+        goWebsite(urllabel, url);
 
         url ="https://github.com/vlsi/ksar/CONTRIBUTION.md";
         authorlabel.setText(String.format("<html> Authors    :   <a href=\"\">%s</a></html>", url ));
+        authorlabel.setCursor(new Cursor(Cursor.HAND_CURSOR));
         jPanel2.add(authorlabel);
+        goWebsite(authorlabel, url);
 
         licencelabel.setText("License :  BSD (see LICENCE file)");
         jPanel2.add(licencelabel);
 
         url ="http://sourceforge.net/projects/ksar/";
         tipslabel.setText(String.format("<html> Fork of       :   <a href=\"\">%s</a></html>", url ));
+        tipslabel.setCursor(new Cursor(Cursor.HAND_CURSOR));
         jPanel2.add(tipslabel);
+        goWebsite(tipslabel, url);
 
         getContentPane().add(jPanel2);
 
@@ -121,7 +135,18 @@ public class AboutBox extends javax.swing.JDialog {
         dispose();
     }//GEN-LAST:event_OkButtonActionPerformed
 
-   
+    private void goWebsite(JLabel website, String url) {
+        website.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                try {
+                    Desktop.getDesktop().browse(new URI(url));
+                } catch (URISyntaxException | IOException ex) {
+                    //It looks like there's a problem
+                }
+            }
+        });
+    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton OkButton;
