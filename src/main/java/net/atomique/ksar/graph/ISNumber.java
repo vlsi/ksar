9fa6d36cebd046c30916f386eacaee70d87b056a
Peter Pitterling <peter.pitterling@sap.com>
2017-05-10 08:27:30 +0200

    apply checkstyle rules to packages (export|graph)

--- src/main/java/net/atomique/ksar/graph/ISNumber.java
+++ src/main/java/net/atomique/ksar/graph/ISNumber.java
@@ -12,47 +12,49 @@ import java.text.ParsePosition;
 
 public class ISNumber extends NumberFormat {
 
-    private final static double IS_kilo = 1000.0;
-    private final static double IS_mega = 1000000.0;
-    private final static double IS_giga = 1000000000.0;
+  private final static double IS_kilo = 1000.0;
+  private final static double IS_mega = 1000000.0;
+  private final static double IS_giga = 1000000000.0;
 
-/*    public ISNumber() {
-    }*/
+  /*
+    public ISNumber() {
+    }
+  */
 
-    public ISNumber(int value) {
-        kilo = value;
+  public ISNumber(int value) {
+    kilo = value;
+  }
+
+  public StringBuffer format(double number, StringBuffer toAppendTo, FieldPosition pos) {
+    DecimalFormat formatter = new DecimalFormat("#,##0.0");
+
+    if (kilo == 0) {
+      return toAppendTo.append(formatter.format(number));
+    }
+    if ((number * kilo) < IS_kilo) {
+      return toAppendTo.append(formatter.format(number));
+    }
+    if ((number * kilo) < IS_mega) {
+
+      toAppendTo.append(formatter.format((number * kilo) / IS_kilo)).append(" K");
+      return toAppendTo;
+    }
+    if ((number * kilo) < (IS_giga)) {
+      toAppendTo.append(formatter.format((number * kilo) / (IS_mega))).append(" M");
+      return toAppendTo;
     }
 
-    public StringBuffer format(double number, StringBuffer toAppendTo, FieldPosition pos) {
-        DecimalFormat formatter = new DecimalFormat("#,##0.0");
+    toAppendTo.append(formatter.format((number * kilo) / (IS_giga))).append(" G");
+    return toAppendTo;
+  }
 
-        if (kilo == 0) {
-            return toAppendTo.append(formatter.format( number));
-        }
-        if ((number * kilo) < IS_kilo) {
-            return toAppendTo.append(formatter.format( number));
-        }
-        if ((number * kilo) < IS_mega) {
+  public StringBuffer format(long number, StringBuffer toAppendTo, FieldPosition pos) {
+    return format((double) (number * kilo), toAppendTo, pos);
+  }
 
-            toAppendTo.append(formatter.format( (number * kilo) / IS_kilo)).append(" K");
-            return toAppendTo;
-        }
-        if ((number * kilo) < (IS_giga)) {
-            toAppendTo.append(formatter.format((number * kilo) / (IS_mega))).append(" M");
-            return toAppendTo;
-        }
+  public Number parse(String source, ParsePosition parsePosition) {
+    return null;
+  }
 
-        toAppendTo.append(formatter.format((number * kilo) / (IS_giga))).append(" G");
-        return toAppendTo;
-    }
-
-    public StringBuffer format(long number, StringBuffer toAppendTo, FieldPosition pos) {
-        return format((double) (number * kilo), toAppendTo, pos);
-    }
-
-    public Number parse(String source, ParsePosition parsePosition) {
-        return null;
-    }
-    
-    private int kilo = 0;
+  private int kilo = 0;
 }
