546fd1738062f1b3247666a8d872a5784b63f9a2
elkrieg <sajnov.mv@gmail.com>
2017-07-08 03:05:58 +0400

    Update AllParser.java

--- src/main/java/net/atomique/ksar/AllParser.java
+++ src/main/java/net/atomique/ksar/AllParser.java
@@ -20,6 +20,32 @@ import java.util.TreeSet;
 
 public abstract class AllParser {
 
+  protected String sarStartDate = null;
+  protected String sarEndDate = null;
+
+  protected LocalDateTime startofgraph = null;
+  protected LocalDateTime endofgraph = null;
+  protected TreeSet<LocalDateTime> DateSamples = new TreeSet<LocalDateTime>();
+  protected int firstdatacolumn = 0;
+
+  abstract public String getInfo();
+
+  abstract public void parse_header(String s);
+
+  abstract public void updateUITitle();
+
+  protected kSar mysar = null;
+  protected OSConfig myosconfig = null;
+  protected String ParserName = null;
+
+  protected LocalTime parsetime = null;
+  protected LocalDate parsedate = null;
+
+  protected String currentStat = "NONE";
+  protected String dateFormat = "MM/dd/yy";
+  protected String timeFormat = "HH:mm:ss";
+  protected int timeColumn = 1;
+
   private static final Logger log = LoggerFactory.getLogger(AllParser.class);
   private static final Map<String, String> DATE_FORMAT_REGEXPS = new HashMap<String, String>() {
     {
@@ -132,30 +158,4 @@ public abstract class AllParser {
     }
     return null; // Unknown format.
   }
-
-  protected String sarStartDate = null;
-  protected String sarEndDate = null;
-
-  protected LocalDateTime startofgraph = null;
-  protected LocalDateTime endofgraph = null;
-  protected TreeSet<LocalDateTime> DateSamples = new TreeSet<LocalDateTime>();
-  protected int firstdatacolumn = 0;
-
-  abstract public String getInfo();
-
-  abstract public void parse_header(String s);
-
-  abstract public void updateUITitle();
-
-  protected kSar mysar = null;
-  protected OSConfig myosconfig = null;
-  protected String ParserName = null;
-
-  protected LocalTime parsetime = null;
-  protected LocalDate parsedate = null;
-
-  protected String currentStat = "NONE";
-  protected String dateFormat = "MM/dd/yy";
-  protected String timeFormat = "HH:mm:ss";
-  protected int timeColumn = 1;
 }
