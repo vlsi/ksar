301410ace25a844a7fa42b6df1b60b513623b417
Pitt <Pitterling@users.noreply.github.com>
2022-01-05 21:56:57 +0100

    SortedTreeNode - specify Comparable<T>, remove unused functions, add "lo" device, copyright (#250)

--- src/main/java/net/atomique/ksar/ui/SortedTreeNode.java
+++ src/main/java/net/atomique/ksar/ui/SortedTreeNode.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018 The kSAR Project. All rights reserved.
+ * Copyright 2022 The kSAR Project. All rights reserved.
  * See the LICENSE file in the project root for more information.
  */
 
@@ -9,13 +9,15 @@ import java.util.Comparator;
 import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.MutableTreeNode;
 
-public class SortedTreeNode extends DefaultMutableTreeNode implements Comparable {
+public class SortedTreeNode extends DefaultMutableTreeNode implements Comparable<SortedTreeNode> {
 
   public static final long serialVersionUID = 15071L;
 
   private static final Comparator<String> comparator =
       Comparator.nullsFirst(
-          Comparator.<String, Boolean>comparing("all"::equals).thenComparing("sum"::equals)
+          Comparator.<String, Boolean>comparing("all"::equals)
+              .thenComparing("sum"::equals)
+              .thenComparing("lo"::equals)
               .reversed()
               .thenComparing(NaturalComparator.INSTANCE));
 
@@ -31,44 +33,14 @@ public class SortedTreeNode extends DefaultMutableTreeNode implements Comparable
     super(tmp);
   }
 
-  /*
-  public SortedTreeNode (GraphDescription graphdesc) {
-      super(graphdesc);
-  }
-  */
-
-  public void count_graph(SortedTreeNode node) {
-    int num = node.getChildCount();
-
-    if (num > 0) {
-      for (int i = 0; i < num; i++) {
-        SortedTreeNode l = (SortedTreeNode) node.getChildAt(i);
-        count_graph(l);
-      }
-    } else {
-      Object obj1 = node.getUserObject();
-      if (obj1 instanceof TreeNodeInfo) {
-        leaf_num++;
-      }
-    }
-
-  }
-
-  public int LeafCount() {
-    leaf_num = 0;
-    count_graph(this);
-    return leaf_num;
-  }
-
   @Override
   public void insert(final MutableTreeNode newChild, final int childIndex) {
     super.insert(newChild, childIndex);
     this.children.sort(null);
   }
 
-  public int compareTo(final Object o) {
+  public int compareTo(final SortedTreeNode o) {
     return comparator.compare(this.toString(), o.toString());
   }
 
-  private int leaf_num = 0;
 }
