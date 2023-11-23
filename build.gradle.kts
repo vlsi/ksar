964556f91e3581c9c5ad8fa3ead73301b77d7ec8
Pitt <Pitterling@users.noreply.github.com>
2023-11-23 08:01:57 +0100

    Gradle 8.2 + plugins + Java11 + kSAR version 6.0.0 (#284)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 plugins {
     id("java")
     id("application")
-    id("com.github.johnrengelman.shadow") version "7.1.2"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
     id("checkstyle")
-    id("com.github.vlsi.gradle-extensions") version "1.84"
+    id("com.github.vlsi.gradle-extensions") version "1.90"
 }
 
 group = "com.github.vlsi.ksar"
@@ -17,8 +17,8 @@ println("Building kSar $version")
 setProperty("archivesBaseName", "ksar")
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
 }
 
 application {
@@ -31,8 +31,8 @@ repositories {
 
 dependencies {
     implementation("org.slf4j:slf4j-api:2.0.6")
-    implementation("ch.qos.logback:logback-core:1.3.5")
-    implementation("ch.qos.logback:logback-classic:1.3.5")
+    implementation("ch.qos.logback:logback-core:1.3.7")
+    implementation("ch.qos.logback:logback-classic:1.3.7")
 
     implementation("com.itextpdf:itextpdf:5.5.13.3")
     implementation("com.jcraft:jsch:0.1.55")
